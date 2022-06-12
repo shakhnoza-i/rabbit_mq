6cdbf5d5aaf2d07d7ee0d8673142990568fd7b4f
shaha <shaha0111@mail.ru>
2022-06-12 23:42:18 +0600

    1 commit

---
+++ message_broker.py
@@ -0,0 +1,9 @@
+"""
+like tcp - rabbitmq
+udp - redis
+
+Manage the data of all devices. Just send all your data of these devices towards
+your Message Broker, and it will take care of processing them.
+
+Registration with the third party services
+"""
